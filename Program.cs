0e1d56aedc834076b333d77f19d44f90c7d14db4
Dan Collins <dan.collins.dev@gmail.com>
2024-12-18 16:22:57 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿/* 
+    Purpose of Project: Dig into strings
+
+    Note to self from Documentation:
+
+        String objects are immutable: they can't be changed after they're created. All of the String methods and C# operators that appear to modify a string actually return the results in a new string object.
+
+        string "modification" is actually a new string creation, you must use caution when you create references to strings. If you create a reference to a string, and then "modify" the original string, the reference continues to point to the original object instead of the new object that was created when the string was modified.
+
+ */
+using System;
+
+namespace c_sharp_strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myFriendsName = "Troy";
+
+            // concatenation
+            Console.WriteLine("Hello " + myFriendsName!);
+
+            // interpolation 
+            Console.WriteLine($"Hello {myFriendsName}!");
+
+            string otherFriendsName = @"Abed";
+
+            // verbatim literal with string interpolation
+            string message = @$"
+                                {myFriendsName} and {otherFriendsName} in the morrrrrning!";
+            
+            
+            Console.WriteLine(message);
+
+
+            // Using the string object method to concatenate message above
+            string concatWithStringMethod = String.Concat(myFriendsName, " and ", otherFriendsName, " in the morrrrrning!");
+            Console.WriteLine(concatWithStringMethod);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Strings in C#
+
+## Purpose of Project
+To learn the basics of strings in C#. This serves as an introduction and quick reference to the string object in C# and related methods.
+
+## Instructions
+1. If not already installed, navigate to the [official installation documentation](https://learn.microsoft.com/en-us/dotnet/core/install/) to run this app.
+2. Clone the repo to your local machine.
+3. Navigate to the root of the project directory in your terminal and run `dotnet run`.
